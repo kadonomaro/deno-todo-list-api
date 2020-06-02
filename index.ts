7e22d4ae6cb250cdea5a7c539725b59d4f149b6d
kadonomaro <denis.afer@gmail.com>
2020-06-02 19:17:55 +0700

    feat: add base get routing

--- index.ts
+++ index.ts
@@ -1,14 +1,40 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { IItem } from "./interfaces/item.interface.ts";
 
 const port = 8000;
 const app = new Application();
 const router = new Router();
 
-router.get('/', ({ response }) => {
-    response.body = 'Hello Deno';
-});
+const todos: Array<IItem> = [
+    {id: '1', title: 'First', isComplete: false},
+    {id: '2', title: 'Second', isComplete: false},
+    {id: '3', title: 'Third', isComplete: true}
+]
+
+router
+    .get('/api/todos', ({ response }) => {
+        response.status = 200;
+        response.body = {
+            todos
+        };
+    })
+    .get('/api/todos/:id', ({ response, params }) => {
+        const item: IItem | undefined = todos.find(item => item.id === params.id);
+        if (item) {
+            response.status = 200;
+            response.body = {
+                item
+            }
+        } else {
+            response.status = 404;
+            response.body = {
+                message: 'Item not found'
+            }
+        }
+    })
 
 app.use(router.routes());
 
+console.log('Server has been started on port: ', port);
 await app.listen({ port });
 
